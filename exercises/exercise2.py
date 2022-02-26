2fad93496e5d391957e566b467e878f027c7b7d4
Benjamin Hays <benjaminhays.02@gmail.com>
2022-02-26 14:02:00 -0600

    CAC 102 - 2 - My Application Overview Exercise

---
+++ exercises/exercise2.py
@@ -0,0 +1,40 @@
+# This is the solution to problem 1
+# I assigned different messages to each variable and printed them
+# within the simple function
+def simple():
+    name1 = "Warriors"
+    name2 = "Thunder"
+    name3 = "Pelicans"
+    name4 = "Heat"
+    name5 = "Raptors\n"
+    print(name1)
+    print(name2)
+    print(name3)
+    print(name4)
+    print(name5)
+
+simple()
+
+# This is the solution to problem 2
+# Value 1 is assigned various messages and the simple 2 function is
+# recalled to display those messages.
+def simple2():
+    value1 = "Shockers"
+    print(value1)
+    value1 = "Jayhawks"
+    print(value1)
+    value1 = "Wildcats"
+    print(value1)
+    value1 = "Bobcats\n"
+    print(value1)
+
+simple2()
+
+# This is the solution to problem 3
+# Passing an argument to print inside a function
+# The argument in this case is Christmas
+def message(arg1):
+    print(arg1)
+
+message('Christmas')
+
